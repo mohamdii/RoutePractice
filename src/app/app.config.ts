48b69851673f211fcacb0eeb4597b94fc9f88b5d
mohamdii <122834211+mohamdii@users.noreply.github.com>
2025-04-09 09:16:05 +0200

    Commiting Service

---
+++ src/app/app.config.ts
@@ -0,0 +1,10 @@
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { appRoutes } from "./app.routes";
+
+export const appConfig:ApplicationConfig = {
+  providers: [
+    provideRouter(appRoutes, withComponentInputBinding()),
+  ]
+}
+
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    component: NoTaskComponent
+  },
+  {
+    path: "users/:userId",
+    component: UserTasksComponent
+  }
+]
